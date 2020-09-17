e71f97d66e729da04baf6821a3bee0be220ccb09
M4lal0 <malalocerrillo@gmail.com>
2020-09-16 20:00:44 -0500

    feat: Updates

--- files/scripts/vpnstatus.sh
+++ files/scripts/vpnstatus.sh
@@ -4,13 +4,7 @@ IFACE="tun0"
 ip=$(/usr/sbin/ifconfig $IFACE 2>/dev/null | grep "inet " | awk '{print $2}')
 
 if [ "$ip" != "" ]; then
-	if [ "$1" == "--copy" ]; then
-		echo -n "$ip" | xclip -sel clip
-		notify-send "Copied!"
-		exit
-	else
-		echo " $ip"
-	fi
+	echo " $ip"
 else
 	echo " "
 fi
