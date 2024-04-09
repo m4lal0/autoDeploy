f50c02367076515690c3ef292175c3a17c1fc9f6
Mario <malalocerrillo@gmail.com>
2024-04-09 11:09:16 -0600

    feat: update
    CHANGES:
    - Modify CPU and RAM scripts

--- files/scripts/cpustatus.sh
+++ files/scripts/cpustatus.sh
@@ -4,11 +4,11 @@ CPU=$(top -bn 2 -d 0.2 | grep '^%Cpu' | tail -n 1 | awk '{printf "%02d", $2+$4+$
 
 if [ ${CPU} -gt 90 ]; then
         echo "<txt> 󰻠 <span foreground=\"red\">$CPU </span></txt>"
-		echo "<tool>RAM info</tool>"
+		echo "<tool>CPU info</tool>"
 elif [ ${CPU} -gt 70 ]; then
         echo "<txt> 󰻠 <span foreground=\"yellow\">$CPU </span></txt>"
-		echo "<tool>RAM info</tool>"
+		echo "<tool>CPU info</tool>"
 else
         echo "<txt> 󰻠 $CPU </txt>"
-		echo "<tool>RAM info</tool>"
+		echo "<tool>CPU info</tool>"
 fi
--- files/scripts/ramstatus.sh
+++ files/scripts/ramstatus.sh
@@ -5,11 +5,11 @@ FREE_MEM=$(free -h --giga | awk '/^Mem:/ {print $3 "/" $2}')
 
 if [ ${USED_MEM} -gt 5 ]; then
         echo "<txt>  <span foreground=\"red\">$FREE_MEM </span></txt>"
-        echo "<tool>RAM INFO</tool>"
+        echo "<tool>RAM info</tool>"
 elif [ ${USED_MEM} -gt 2 ]; then
         echo "<txt>  <span foreground=\"yellow\">$FREE_MEM </span></txt>"
-        echo "<tool>RAM INFO</tool>"
+        echo "<tool>RAM info</tool>"
 else
         echo "<txt>  $FREE_MEM </txt>"
-        echo "<tool>RAM INFO</tool>"
+        echo "<tool>RAM info</tool>"
 fi
